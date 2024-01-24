15f22987c5203fa8970487811a380ea4f183b46e
Drew <drew@Drews-MacBook-Pro.local>
2024-01-23 16:47:21 -0800

    posts wip

--- client/src/App.js
+++ client/src/App.js
@@ -13,11 +13,17 @@ function App() {
           <a href="">Register</a>
         </nav>
       </header>
-      <div className="entry">
-        <img src={reactpic} alt="" />
+      <div className="post">
+        <div className="image">
+          <img src={reactpic} alt="" />
+        </div>
         <div className="texts">
           <h2>Redux, why?</h2>
-          <p>
+          <p className="info">
+            <span className="author">Drew Gomez</span>
+            <time>2024-01-23 4:42pm</time>
+          </p>
+          <p className="summary">
             Let's take a second to dive in and decide why or why not we'd be
             going with Redux in our next project. We will talk about pros and
             cons, and also mention some competitors who I've also broken down,
@@ -25,11 +31,35 @@ function App() {
           </p>
         </div>
       </div>
-      <div className="entry">
-        <img src={reactpic} alt="" />
+      <div className="post">
+        <div className="image">
+          <img src={reactpic} alt="" />
+        </div>
         <div className="texts">
           <h2>Redux, why?</h2>
-          <p>
+          <p className="info">
+            <span className="author">Drew Gomez</span>
+            <time>2024-01-23 4:42pm</time>
+          </p>
+          <p className="summary">
+            Let's take a second to dive in and decide why or why not we'd be
+            going with Redux in our next project. We will talk about pros and
+            cons, and also mention some competitors who I've also broken down,
+            links below!
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src={reactpic} alt="" />
+        </div>
+        <div className="texts">
+          <h2>Redux, why?</h2>
+          <p className="info">
+            <span className="author">Drew Gomez</span>
+            <time>2024-01-23 4:42pm</time>
+          </p>
+          <p className="summary">
             Let's take a second to dive in and decide why or why not we'd be
             going with Redux in our next project. We will talk about pros and
             cons, and also mention some competitors who I've also broken down,
