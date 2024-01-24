05548ebb64af5e57f45b6adbf2a2ca8025432dc9
Drew <drew@Drews-MacBook-Pro.local>
2024-01-23 16:31:12 -0800

    initial posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,42 @@
 import "./App.css";
+import reactpic from "./assets/react.jpg";
 
 function App() {
   return (
     <main>
       <header>
         <a href="" className="logo">
-          My Blog
+          Qodebro Drew
         </a>
         <nav>
           <a href="">Login</a>
           <a href="">Register</a>
         </nav>
       </header>
+      <div className="entry">
+        <img src={reactpic} alt="" />
+        <div className="texts">
+          <h2>Redux, why?</h2>
+          <p>
+            Let's take a second to dive in and decide why or why not we'd be
+            going with Redux in our next project. We will talk about pros and
+            cons, and also mention some competitors who I've also broken down,
+            links below!
+          </p>
+        </div>
+      </div>
+      <div className="entry">
+        <img src={reactpic} alt="" />
+        <div className="texts">
+          <h2>Redux, why?</h2>
+          <p>
+            Let's take a second to dive in and decide why or why not we'd be
+            going with Redux in our next project. We will talk about pros and
+            cons, and also mention some competitors who I've also broken down,
+            links below!
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
