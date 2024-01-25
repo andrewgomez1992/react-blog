01f29d16101dca017f432129a85d84e4e567869d
Drew <drew@Drews-MacBook-Pro.local>
2024-01-24 22:05:36 -0800

    login wip, cookie, jwt set

--- api/index.js
+++ api/index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const app = express();
+
+const salt = bcrypt.genSaltSync(10);
+const secret = "qsdu98qw12319812kln";
 
 app.use(cors());
 app.use(express.json());
@@ -12,11 +17,43 @@ mongoose.connect(
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  const userDoc = await User.create({
-    username,
-    password,
-  });
-  res.json(userDoc);
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+
+  if (!userDoc) {
+    // User not found
+    return res.status(400).json("User not found");
+  }
+
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+
+  if (passOk) {
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+
+      // Set the cookie before sending the JSON response
+      res.cookie("token", token);
+
+      // Log in successful
+      // Additional login logic can be added here
+      res.json({ success: true });
+    });
+  } else {
+    // Wrong credentials
+    res.status(400).json("Wrong credentials");
+  }
 });
 
 app.listen(4000, () => {
