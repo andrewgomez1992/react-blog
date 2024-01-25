488faebbcae43d3f98c792f0658f2444763ad420
Drew <drew@Drews-MacBook-Pro.local>
2024-01-24 21:23:15 -0800

    mongo db connected

--- api/index.js
+++ api/index.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/User");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+mongoose.connect(
+  "mongodb+srv://blog:Spanish8000@cluster0.euzg2jo.mongodb.net/"
+);
 
-app.post("/register", (req, res) => {
-  // Handle registration logic here
-  console.log(req.body);
-  res.json("Registration successful");
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.create({
+    username,
+    password,
+  });
+  res.json(userDoc);
 });
 
 app.listen(4000, () => {
