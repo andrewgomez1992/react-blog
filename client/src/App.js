3725f8b4671c6f1e740363fc1708a254b0ab95e7
Drew <drew@Drews-MacBook-Pro.local>
2024-01-24 20:32:26 -0800

    register and login intial pages added

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import Post from "./components/Post";
 import Layout from "./Layout";
+import Homepage from "./pages/Homepage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Post />} />
-          <Route path={"/login"} element={<div>login page</div>} />
+          <Route index element={<Homepage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Route>
       </Routes>
     </>
