3019b078298ea744fb4e559d5fd835185cb56900
Drew <drew@Drews-MacBook-Pro.local>
2024-01-26 19:14:46 -0800

    getting cookie from profile endpoint

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Header = () => {
+  useEffect(() => {
+    axios.get("http://localhost:4000/profile", {
+      withCredentials: true,
+    });
+  }, []);
+
   return (
     <header>
       <Link to="/" className="logo">
