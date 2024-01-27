03aeb3c69bbde52abb4727e86d11abf4370695fe
Drew <drew@Drews-MacBook-Pro.local>
2024-01-26 21:29:47 -0800

    wip

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -2,45 +2,78 @@ import React, { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, 3, 4, 5, 6, false] }],
+    //   [{ font: [] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ indent: "-1" }, { indent: "+1" }],
+    ["link", "image"],
+    [({ color: [] }, { background: [] })],
+
+    ["clean"], // remove formatting button
+  ],
+};
+
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+];
+
 const CreatePost = () => {
   const [title, setTitle] = useState("");
   const [summary, setSummary] = useState("");
   const [content, setContent] = useState("");
-  const modules = {
-    toolbar: [
-      [{ header: [1, 2, 3, 4, 5, 6, false] }],
-      //   [{ font: [] }],
-      ["bold", "italic", "underline", "strike", "blockquote"],
-      [{ list: "ordered" }, { list: "bullet" }],
-      [{ indent: "-1" }, { indent: "+1" }],
-      ["link", "image"],
-      [({ color: [] }, { background: [] })],
+  const [files, setFile] = useState("");
 
-      ["clean"], // remove formatting button
-    ],
+  const createNewPost = async (e) => {
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("file", files[0]);
+    e.preventDefault();
+    console.log("files", files);
+
+    const response = await fetch("http://localhost:4000/post", {
+      method: "POST",
+      body: data,
+    });
+    console.log(await response.json());
   };
 
-  const formats = [
-    "header",
-    "bold",
-    "italic",
-    "underline",
-    "strike",
-    "blockquote",
-    "list",
-    "bullet",
-    "indent",
-    "link",
-    "image",
-  ];
-
   return (
-    <form>
-      <input type="title" placeholder="Title" />
-      <input type="summary" placeholder="Summary" />
-      <input type="file" />
-      <ReactQuill value={content} modules={modules} formats={formats} />
-      <button style={{ marginTop: "5px" }}></button>
+    <form onSubmit={createNewPost}>
+      <input
+        type="title"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder="Summary"
+        value={summary}
+        onChange={(e) => setSummary(e.target.value)}
+      />
+      <input type="file" onChange={(e) => setFile(e.target.files)} />
+      <ReactQuill
+        value={content}
+        modules={modules}
+        formats={formats}
+        onChange={(newValue) => setContent(newValue)}
+      />
+      <button style={{ marginTop: "5px" }}>Create New Post</button>
     </form>
   );
 };
