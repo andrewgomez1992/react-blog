f71fce08b5b1737cd6f53d162453282e5f2bbe88
Drew <drew@Drews-MacBook-Pro.local>
2024-01-28 20:50:40 -0800

    posts done

--- api/index.js
+++ api/index.js
@@ -20,6 +20,7 @@ mongoose.connect(
   "mongodb+srv://blog:Spanish8000@cluster0.euzg2jo.mongodb.net/"
 );
 app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
@@ -29,35 +30,27 @@ app.post("/register", async (req, res) => {
       password: bcrypt.hashSync(password, salt),
     });
     res.json(userDoc);
-  } catch (err) {
-    res.status(400).json(err);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json(e);
   }
 });
 
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
   const userDoc = await User.findOne({ username });
-
-  if (!userDoc) {
-    // User not found
-    return res.status(400).json("User not found");
-  }
-
   const passOk = bcrypt.compareSync(password, userDoc.password);
-
   if (passOk) {
+    // logged in
     jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
       if (err) throw err;
-
-      // Set the cookie before sending the JSON response
-      res.cookie("token", token);
-
-      // Log in successful
-      res.json({ success: true, id: userDoc._id, username });
+      res.cookie("token", token).json({
+        id: userDoc._id,
+        username,
+      });
     });
   } else {
-    // Wrong credentials
-    res.status(400).json("Wrong credentials");
+    res.status(400).json("wrong credentials");
   }
 });
 
@@ -67,7 +60,6 @@ app.get("/profile", (req, res) => {
     if (err) throw err;
     res.json(info);
   });
-  res.json(token);
 });
 
 app.post("/logout", (req, res) => {
@@ -78,23 +70,67 @@ app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
   const { originalname, path } = req.file;
   const parts = originalname.split(".");
   const ext = parts[parts.length - 1];
-  const newPath = path + "" + ext;
+  const newPath = path + "." + ext;
   fs.renameSync(path, newPath);
 
-  const { title, summary, content, coverImage } = req.body;
-  const postDoc = await Post.create({
-    title,
-    summary,
-    content,
-    coverImage: newPath,
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { title, summary, content } = req.body;
+    const postDoc = await Post.create({
+      title,
+      summary,
+      content,
+      coverImage: newPath,
+      author: info.id,
+    });
+    res.json(postDoc);
   });
-
-  res.json(postDoc);
 });
 
+// app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
+//   let newPath = null;
+//   if (req.file) {
+//     const { originalname, path } = req.file;
+//     const parts = originalname.split(".");
+//     const ext = parts[parts.length - 1];
+//     newPath = path + "." + ext;
+//     fs.renameSync(path, newPath);
+//   }
+
+//   const { token } = req.cookies;
+//   jwt.verify(token, secret, {}, async (err, info) => {
+//     if (err) throw err;
+//     const { id, title, summary, content } = req.body;
+//     const postDoc = await Post.findById(id);
+//     const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+//     if (!isAuthor) {
+//       return res.status(400).json("you are not the author");
+//     }
+//     await postDoc.update({
+//       title,
+//       summary,
+//       content,
+//       coverImage: newPath ? newPath : postDoc.coverImage,
+//     });
+
+//     res.json(postDoc);
+//   });
+// });
+
 app.get("/post", async (req, res) => {
-  const posts = await Post.find();
-  res.json(posts);
+  res.json(
+    await Post.find()
+      .populate("author", ["username"])
+      .sort({ createdAt: -1 })
+      .limit(20)
+  );
+});
+
+app.get("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  const postDoc = await Post.findById(id).populate("author", ["username"]);
+  res.json(postDoc);
 });
 
 app.listen(4000, () => {
--- api/models/Post.js
+++ api/models/Post.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
+const { v4: uuidv4 } = require("uuid");
 
 const PostSchema = new Schema(
   {
+    postId: {
+      type: String,
+      default: () => uuidv4(), // Generate a new UUID for each document
+      unique: true,
+    },
     title: String,
     summary: String,
     content: String,
     coverImage: String,
+    author: { type: Schema.Types.ObjectId, ref: "User" },
   },
   {
     timestamps: true,
